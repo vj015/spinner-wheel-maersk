25752c84269ba71d224b50914466cec5c89b0be8
Vipul Jain <75699731+vj015@users.noreply.github.com>
2023-10-29 13:34:34 +0530

    Intial Changes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SpinnerWheel msg="Maersk Spinner Wheel" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SpinnerWheel from "./components/SpinnerWheel.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    SpinnerWheel,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
