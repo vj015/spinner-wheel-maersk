9d0bb71befe5192e2f9c279eef8053a15b78a6b0
Vipul Jain <75699731+vj015@users.noreply.github.com>
2023-10-30 19:51:10 +0530

    overlapping determine  method added

--- src/components/SpinnerWheel.vue
+++ src/components/SpinnerWheel.vue
@@ -30,6 +30,20 @@ export default {
     };
   },
   methods: {
+    elementsOverlap() {
+      console.log("function called");
+      const el1 = document.getElementById("arr");
+      const el2 = document.getElementById("arr1");
+      const domRect1 = el1.getBoundingClientRect();
+      const domRect2 = el2.getBoundingClientRect();
+
+      return !(
+        domRect1.top > domRect2.bottom ||
+        domRect1.right < domRect2.left ||
+        domRect1.bottom < domRect2.top ||
+        domRect1.left > domRect2.right
+      );
+    },
     turnWheel() {
       const number = Math.ceil(Math.random() * 1000);
       this.rotationAngle += number;
