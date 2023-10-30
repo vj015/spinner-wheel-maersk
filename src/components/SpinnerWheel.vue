866299f76c63c16db6e3015c977f05449777cdf8
Vipul Jain <75699731+vj015@users.noreply.github.com>
2023-10-30 21:12:04 +0530

    determined game result method added

--- src/components/SpinnerWheel.vue
+++ src/components/SpinnerWheel.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="body">
+    <div class="arrow" id="crane">
+      <img src="../assets/crane-icon.png" alt="Icon" />
+    </div>
     <div
       id="spin"
       :class="{ shipanimation: isSpinning }"
@@ -9,13 +12,13 @@
       class="container"
       :style="{ transform: `rotate(${rotationAngle}deg)` }"
     >
-      <div class="one">1</div>
-      <div class="two">2</div>
-      <div class="three">3</div>
-      <div class="four">4</div>
-      <div class="five">5</div>
-      <div class="six">6</div>
-      <div class="seven">7</div>
+      <div class="one" id="segment-1">1</div>
+      <div class="two" id="segment-2">2</div>
+      <div class="three" id="segment-3">3</div>
+      <div class="four" id="segment-4">4</div>
+      <div class="five" id="segment-5">5</div>
+      <div class="six" id="segment-6">6</div>
+      <div class="seven" id="segment-7">7</div>
     </div>
   </div>
 </template>
@@ -27,13 +30,14 @@ export default {
     return {
       rotationAngle: 0,
       isSpinning: false,
+      count: 0,
+      winner: 0,
+      nowinner: false,
     };
   },
   methods: {
-    elementsOverlap() {
+    elementsOverlap(el1, el2) {
       console.log("function called");
-      const el1 = document.getElementById("arr");
-      const el2 = document.getElementById("arr1");
       const domRect1 = el1.getBoundingClientRect();
       const domRect2 = el2.getBoundingClientRect();
 
@@ -44,13 +48,39 @@ export default {
         domRect1.left > domRect2.right
       );
     },
+    getSegmentElement(index) {
+      return document.getElementById(`segment-${index}`);
+    },
+    getCraneElement() {
+      return document.getElementById("crane");
+    },
+    checkForWinner() {
+      for (let i = 1; i < 8; i++) {
+        const segment = this.getSegmentElement(i);
+        const crane = this.getCraneElement();
+
+        if (this.elementsOverlap(segment, crane)) {
+          this.count++;
+          this.winner = i;
+        }
+      }
+
+      if (this.count > 1) {
+        this.nowinner = true;
+        this.winner = null;
+      } else {
+        this.nowinner = false;
+        console.log("Result Overlapping one", this.winner);
+      }
+    },
     turnWheel() {
       const number = Math.ceil(Math.random() * 1000);
       this.rotationAngle += number;
       this.isSpinning = true;
       setTimeout(() => {
         this.isSpinning = false;
-      }, 1000);
+        this.checkForWinner();
+      }, 2000);
     },
   },
 };
